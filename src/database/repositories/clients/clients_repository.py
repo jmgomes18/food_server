175d9c93d67f139a6ee660834cd3cc83df2ac7cb
João Marco <j.marcogomes18@gmail.com>
2022-10-02 01:56:28 -0300

    chore: refactoring

--- src/database/repositories/clients/clients_repository.py
+++ src/database/repositories/clients/clients_repository.py
@@ -1,4 +1,3 @@
-
 import logging
 from sqlalchemy.exc import InvalidRequestError
 from src.database.entities import Clients
@@ -8,14 +7,22 @@ logger = logging.getLogger(__name__)
 
 class ClientsRepository:
     @classmethod
-    def insert(data):
+    def insert(cls, data):
         with DBConnectionHandler as conn:
             try:
                 values = Clients(**data)
 
-                logger.infor('inserting data')
+                logger.info('inserting data')
                 conn.session.add(values)
                 conn.session.commit()
+
+                return Clients(
+                    id=values.id, 
+                    email=values.email, 
+                    password=values.password, 
+                    name=values.name, 
+                    active=values.active
+                )
             except InvalidRequestError as e:
                 logger.error('crashed inserting data')
                 conn.session.rollback()
